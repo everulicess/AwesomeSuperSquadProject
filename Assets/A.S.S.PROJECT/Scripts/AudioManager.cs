90c12f9ff70d73be191dce1cf9b9b37c5853703c
ComeOnProTM <101826779+ComeOnProTM@users.noreply.github.com>
2023-11-02 16:07:21 +0100

    Revert "Merge branch 'Audio-and-UX' into Groovy-DialogueSystem"
    This reverts commit df8a108b60d47bb321a634bdc55bec1e97ec0b4b, reversing
    changes made to c1e5a593d2974cf2df6a81cef4ab795b16ea6a19.

--- Assets/A.S.S.PROJECT/Scripts/AudioManager.cs
+++ Assets/A.S.S.PROJECT/Scripts/AudioManager.cs
@@ -9,13 +9,11 @@ public class AudioManager : MonoBehaviour
     public void PlayButtonPress()
     {
         buttonPress.Play();
-        Debug.Log("BEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEP");
     }
 
     public void PlayScannerBeep()
     {
         buttonPress.Play();
-        Debug.Log("SCANNER BEEEEEEEEEEEEEEEEEEEEEEEEEEEEEP");
     }
 
 }
