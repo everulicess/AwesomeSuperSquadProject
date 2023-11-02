2823727ff2dc5fe16aa5d58054aefb0b93fd7e49
MrInfinity12 <infinityspacetrk@gmail.com>
2023-11-02 15:47:00 +0100

    Update AudioManager.cs

--- Assets/A.S.S.PROJECT/Scripts/AudioManager.cs
+++ Assets/A.S.S.PROJECT/Scripts/AudioManager.cs
@@ -8,14 +8,14 @@ public class AudioManager : MonoBehaviour
 
     public void PlayButtonPress()
     {
-        buttonPress.Play();
         Debug.Log("BEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEP");
+        buttonPress.Play();
     }
 
     public void PlayScannerBeep()
-    {
-        buttonPress.Play();
+    {      
         Debug.Log("SCANNER BEEEEEEEEEEEEEEEEEEEEEEEEEEEEEP");
+        buttonPress.Play();
     }
 
 }
