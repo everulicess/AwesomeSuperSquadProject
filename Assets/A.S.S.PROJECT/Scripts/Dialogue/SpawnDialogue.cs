2d16830b236944be0faa343297c1490438c2f10b
ComeOnProTM <101826779+ComeOnProTM@users.noreply.github.com>
2023-11-05 22:22:36 +0100

    Groovy I Dialogue Update

--- Assets/A.S.S.PROJECT/Scripts/Dialogue/SpawnDialogue.cs
+++ Assets/A.S.S.PROJECT/Scripts/Dialogue/SpawnDialogue.cs
@@ -17,7 +17,6 @@ public class SpawnDialogue : MonoBehaviour
         Debug.Log("Spawn Textbox");
         DialogueManager.instance.hasTextPrefab = true;
         CreateDialogue();
-        TriggerDialogue();
         /*DialogueManager.instance.checkForDialogue();*/
 /*        GameObject dialogueManager = GameObject.Find("DialogueManager");
         DialogueManager dm = dialogueManager.GetComponent<DialogueManager>();
